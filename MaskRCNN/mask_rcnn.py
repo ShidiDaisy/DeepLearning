fbfdca8f614eeaade6a55662836634e923999f01
Shidi Yang <nicebling@sina.com>
2020-02-02 23:11:25 +0800

    modified:   .gitignore 	modified:   LogisticRegression/logistic_regression.py 	modified:   MaskRCNN/.idea/MaskRCNN.iml 	modified:   MaskRCNN/.idea/misc.xml 	new file:   MaskRCNN/mask-rcnn-coco/colors.txt 	new file:   MaskRCNN/mask-rcnn-coco/mask_rcnn_inception_v2_coco_2018_01_28.pbtxt 	new file:   MaskRCNN/mask-rcnn-coco/object_detection_classes_coco.txt 	modified:   MaskRCNN/mask_rcnn.py 	new file:   MultivariateTimeSeriesForcastingLSTM/SoptAI_Sales_Prediction.key 	modified:   README.md

--- MaskRCNN/mask_rcnn.py
+++ MaskRCNN/mask_rcnn.py
@@ -5,5 +5,5 @@ import cv2
 import os
 # $ python mask_rcnn_video.py --input videos/slip_and_slide.mp4 \
 # 	--output output/slip_and_slide_output.avi --mask-rcnn mask-rcnn-coco
-mask_rcnn =
-labelsPath = os.path.sep.join()
+mask_rcnn = "mask-rcnn-coco"
+labelsPath = os.path.sep.join([mask_rcnn, "object_detection_classes_coco.txt"])
