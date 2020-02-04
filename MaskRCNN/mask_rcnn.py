3ac122733fe2202b29ec0eabfdb41cf671709b2a
shidi <shidi@elixirtech.com>
2020-02-04 12:12:38 +0800

    modified:   .gitignore 	modified:   LogisticRegression/logistic_regression.py 	deleted:    MaskRCNN/.idea/.gitignore 	deleted:    MaskRCNN/.idea/MaskRCNN.iml 	deleted:    MaskRCNN/.idea/inspectionProfiles/profiles_settings.xml 	deleted:    MaskRCNN/.idea/misc.xml 	deleted:    MaskRCNN/.idea/modules.xml 	deleted:    MaskRCNN/.idea/vcs.xml 	modified:   MaskRCNN/mask_rcnn.py 	modified:   README.md

--- MaskRCNN/mask_rcnn.py
+++ MaskRCNN/mask_rcnn.py
@@ -3,7 +3,86 @@ import random
 import time
 import cv2
 import os
-# $ python mask_rcnn_video.py --input videos/slip_and_slide.mp4 \
-# 	--output output/slip_and_slide_output.avi --mask-rcnn mask-rcnn-coco
-mask_rcnn =
-labelsPath = os.path.sep.join()
+import imutils
+
+if __name__ == '__main__':
+
+    # $ python mask_rcnn_video.py --input videos/slip_and_slide.mp4 \
+    # 	--output output/slip_and_slide_output.avi
+    mask_rcnn = "mask-rcnn-coco"
+    input = "video/HorsesEatingGrass.mp4"
+    confidence_thre = 0.5
+    threshold = 0.5 # minimum threshold for pixel-wise mask segmentation
+    writer = None
+
+    labelsPath = os.path.sep.join([mask_rcnn, "object_detection_classes_coco.txt"])
+    colorsPath = os.path.sep.join([mask_rcnn, "colors.txt"])
+    LABELS = open(labelsPath).read().strip().split("\n")
+    # initialize a list of colors to represent each possible class label
+    np.random.seed(42)
+    COLORS = np.random.randint(0, 255, size=(len(LABELS), 3), dtype="uint8")
+    weightPath = os.path.sep.join([mask_rcnn, "frozen_inference_graph.pb"])
+    configPath = os.path.sep.join([mask_rcnn, "mask_rcnn_inception_v2_coco_2018_01_28.pbtxt"])
+
+    net = cv2.dnn.readNetFromTensorflow(weightPath, configPath)
+    vs = cv2.VideoCapture(input)
+
+    # Get the total number of frames in the video files
+    try:
+        prop = cv2.cv.CV_CAP_PROP_FRAME_COUNT if imutils.is_cv2() else cv2.CAP_PROP_FRAME_COUNT
+        total = int(vs.get(prop))
+    except:
+        print("Could not determine number of frames in video")
+        total = -1
+
+    cur_frame = 0
+    while True:
+        (grabbed, frame) = vs.read()
+        if not grabbed:
+            break
+
+        # swapRB: swap the red and blue channel
+        # return: 1. the bounding box coordinates. 2. the pixel-wise segmentation for each specific object
+        blob = cv2.dnn.blobFromImage(frame, swapRB=True, crop=False)
+        net.setInput(blob) # set a new input for the network
+        start = time.time()
+        (boxes, masks) = net.forward(["detection_out_final", "detection_masks"]) # Runs forward pass to compute output of layer
+        end = time.time()
+
+        # loop detected objects
+        for i in range(0, boxes.shape[2]):
+            classID = int(boxes[0, 0, i, 1])
+            confidence = boxes[0, 0, i, 2]
+            if confidence > confidence_thre:
+                (H, W) = frame.shape[:2]
+                box = boxes[0, 0, i, 3:7] * np.array([W, H, W, H])
+                (startX, startY, endX, endY) = box.astype("int")
+                boxW = endX - startX
+                boxH = endY - startY
+                mask = masks[i, classID]
+                mask = cv2.resize(mask, (boxW, boxH), interpolation=cv2.INTER_NEAREST)
+                mask = (mask > threshold)
+                roi = frame[startY:endY, startX:endX][mask] # extract roi
+
+                color = COLORS[classID]
+                blended = ((0.4 * color) + (0.6 * roi)).astype("uint8")
+                frame[startY:endY, startX:endX][mask] = blended
+
+                # draw bounding box
+                color = [int(c) for c in color]
+                cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
+                text = "{}:{:.4f}".format(LABELS[classID], confidence)
+                cv2.putText(frame, text, (startX, startY-5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+
+        if writer is None:
+            fourcc = cv2.VideoWriter_fourcc(*"MJPG")
+            writer = cv2.VideoWriter(input.replace(".mp4", "-det.mp4"), fourcc, 30, (frame.shape[1], frame.shape[0]), True)
+
+        cur_frame+=1
+        print("Current progress {}%".format(cur_frame/total * 100))
+        writer.write(frame)
+
+    print("[INFO] cleaning up...")
+    writer.release()
+    vs.release()
+
--- README.md
+++ README.md
@@ -6,11 +6,18 @@ Deep learning study notes
 
 You can automatically segment and construct pixel-wise masks for every object in an image, thereby to segment the foreground object from the background. We just need perform <b>instance segmentation</b> using the Mask-RCNN architecture.
 
-#### Difference between semantic segmentation and instance segmentation
+### Difference between semantic segmentation and instance segmentation
 <b>Semantic Segmentation:</b> require us to associate every pixel in an input image with a class label (including background).
 <b>Instance Segmentation:</b> compute a pixel-wise mask for every object in the image, even if the objects in the same class.
 
 The Mask R-CNN we are using here were trained on the COCO dataset, which has L=90, thus the resulting volumn size from the mask module of the Mask R-CNN is 100*90*15*15
+
+### Configuration
+* mask-rcnn-coco/ : The Mask R-CNN model files
+    * frozen_inference_graph.pb(https://github.com/floydhub/object-detection-template/blob/master/models/ssdlite_mobilenet_v2_coco_2018_05_09/frozen_inference_graph.pb): The Mask R-CNN model weights. The weights are pre-trained on the COCO dataset.
+    * mask_rcnn_inception_v2_coco_2018_01_28.pbtxt(https://www.google.com/search?client=safari&rls=en&q=mask_rcnn_inception_v2_coco_2018_01_28.pbtxt&ie=UTF-8&oe=UTF-8): The Mask R-CNN model configuration.
+    * object_detection_classes_coco.txt(https://github.com/opencv/opencv/blob/master/samples/data/dnn/object_detection_classes_coco.txt) : All 90 classes are listed in this text file, one per line. Open it in a text editor to see what objects our model can recognize.
+    * colors.txt(https://github.com/spmallick/learnopencv/blob/master/Mask-RCNN/colors.txt) : This text file contains colors to randomly assign to objects found in the image.
 
 ## RNN
 1. Udemy: Deep Learning A-Z™: Hands-On Artificial Neural Networks
